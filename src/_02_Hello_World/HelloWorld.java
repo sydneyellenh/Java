0cd3f9436268e2e0891cafb4867dbf8a8dfce34e
Sydney Haggard <sydneyellen1996@gmail.com>
2016-09-26 10:04:02 -0400

    Initial commit

---
+++ src/_02_Hello_World/HelloWorld.java
@@ -0,0 +1,11 @@
+//every Java script will have this
+
+package _02_Hello_World;
+
+public class HelloWorld 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+	}
+}
